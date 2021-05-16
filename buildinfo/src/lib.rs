f35ff686e10db99ef24d0b8da109a00f0f9dea7a
Mikail Bagishov <bagishov.mikail@yandex.ru>
2021-05-16 13:59:30 +0300

    buildinfo: report version

--- buildinfo/src/lib.rs
+++ buildinfo/src/lib.rs
@@ -6,6 +6,7 @@ use serde::Serialize;
 pub struct BuildInfo {
     pub build_date: Option<&'static str>,
     pub git_revision: Option<&'static str>,
+    pub version: Option<&'static str>,
 }
 
 impl BuildInfo {
@@ -14,6 +15,7 @@ impl BuildInfo {
         if option_env!("JJS_BUILD_INFO_VERIFY_FULL").is_some() {
             assert!(v.build_date.is_some());
             assert!(v.git_revision.is_some());
+            assert!(v.version.is_some());
         }
         v
     }
@@ -22,11 +24,15 @@ impl BuildInfo {
         BuildInfo {
             build_date: option_env!("JJS_BUILD_INFO_DATE"),
             git_revision: option_env!("JJS_BUILD_INFO_COMMIT"),
+            version: option_env!("JJS_BUILD_INFO_VERSION"),
         }
     }
 
     pub fn wrap_clap<'a>(app: clap::App<'a>) -> clap::App<'a> {
-        static STRING_VERSION: Lazy<String> = Lazy::new(|| format!("{:#?}", BuildInfo::get()));
-        app.version(STRING_VERSION.as_str())
+        static STRING_LONG_VERSION: Lazy<String> = Lazy::new(|| format!("{:#?}", BuildInfo::get()));
+        static STRING_SHORT_VERSION: Lazy<String> =
+            Lazy::new(|| BuildInfo::get().version.unwrap_or("dev").to_string());
+        app.version(STRING_SHORT_VERSION.as_str())
+            .long_version(STRING_LONG_VERSION.as_str())
     }
 }
